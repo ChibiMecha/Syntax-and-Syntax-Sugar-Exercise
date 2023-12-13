5acea99b83bc42744883354cfb691cf2bed04589
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-13 14:14:02 -0600

    Used inferred typing, string interpolation, and ternary operator to write more with less and make code more concise and readable

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
+//     <!-- ## Exercise 1:
+// Given the following code, change the code to implement:
+
+// + inferred typing
+// + string interpolation
+// + and the ternary operator
+// ```
+// int answer = 4;
+// string response;
+// if (answer < 9) 
+// {
+//     response = answer + " is less than nine";
+// }
+// else
+// {
+//     response = answer + " is greater than or equal to nine";
+// }
+// ``` -->
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Select a number:1-9");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
         }
     }
 }
